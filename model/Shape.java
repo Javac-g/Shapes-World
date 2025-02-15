abda11b7f14a5295b54f8fbc3569f32e223709cc
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2025-02-16 01:46:34 +0200

    Update Shape.java
    Still need to add Skills, Interest points for each talent for couples

--- model/Shape.java
+++ model/Shape.java
@@ -1,19 +1,8 @@
-public class Shape{
-private String name;
-protected Boolean single;
+public  abstact class Shape{
 
+    private String name;
+    protected Boolean single;
+    protected Type type; // please add angles count into constructor for each shape type like Type.triangle(3)
+    protected Set<Talent> talents = new HashSet<>(); 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
