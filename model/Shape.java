98747fd08294450c39de602b7dcce5d60d2e21cf
Alexa <AlexaPanashenko@proton.me>
2025-02-20 19:24:16 +0200

    Update Shape.java
    minor changes - Shape extension

--- model/Shape.java
+++ model/Shape.java
@@ -4,8 +4,9 @@ public  abstact class Shape{
     protected Boolean single;
     protected Type type; // please add angles count into constructor for each shape type like Type.triangle(3)
     protected Set<Talent> talents = new HashSet<>(); 
-	protected int evolution;
+	protected int evolution_stage,points_to_evolve;
 	protected Shape couple;
 	protected int funds;
+	protected UUID id;
 
 }
